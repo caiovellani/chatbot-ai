277ef19fa99d0b6a2fe8bf5c567cde6067a3fb7a
Caio Vellani <caiovellani@outlook.com>
2025-05-15 23:12:09 -0300

    feat: Implement basic chat UI layout
    - Add Card component structure for the interface with header, content, and footer sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Card className="w-[440px] h-[700px]">
+        <CardHeader>
+          <CardTitle>Chat AI</CardTitle>
+          <CardDescription>
+            Using Vercel SDK to create a chat bot.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Mesagens</p>
+        </CardContent>
+        <CardFooter>form</CardFooter>
+      </Card>
+    </div>
+  )
 }
